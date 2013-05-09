f191a545d0f93d51621c796e048ad312f3326d81
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-05-09 17:09:29 +0200

    is_inside_comment

--- shared.py
+++ shared.py
@@ -6,15 +6,22 @@ def erase_region(view, edit, region):
 	view.erase(edit, region)
 	return region.begin()
 
-def is_inside_string(view, point):
-	test_region = sublime.Region(point, point)
-	regions = view.find_by_selector("string")
-
+def is_region_inside_regions(test_region, regions):
 	for region in regions:
 		if region.contains(test_region):
 			return region
 
 	return False
+
+def is_inside_string(view, point):
+	test_region = sublime.Region(point, point)
+	regions = view.find_by_selector("string")
+	return is_region_inside_regions(test_region, regions)
+
+def is_inside_comment(view, point):
+	test_region = sublime.Region(point, point)
+	regions = view.find_by_selector("comment")
+	return is_region_inside_regions(test_region, regions)
 
 whitespace_matcher = re.compile("\s*$")
 def is_expression_empty(string):
