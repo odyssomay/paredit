6f6cf4df585ff81a5dfa1d3b62a370003173d2ad
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-05-15 23:29:59 +0200

    removed incorrect test

--- test.py
+++ test.py
@@ -172,7 +172,6 @@ def paredit_test_movement_navigation(view, edit):
 			["(foo |(bar baz) quux)", "(foo (bar baz)| quux)"]
 		,	["(foo (bar baz)|)", "(foo (bar baz))|"]
 		,	["(f|oo (bar baz))", "(foo| (bar baz))"]
-		,	["(fo|o bar)", "(foo  bar|)"]
 		])
 	run_tests(view, edit,
 		"paredit_backward",
