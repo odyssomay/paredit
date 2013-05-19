2221d91f32f097e211472d132c2b66b1c07b09c4
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-05-20 00:39:05 +0200

    paredit_split_sexp implementation

--- miscellaneous.py
+++ miscellaneous.py
@@ -5,8 +5,28 @@ try:
 except:
 	import shared
 
+def insert_split_brackets(view, edit, lbracket, rbracket, point):
+	view.insert(edit, point, rbracket + " " + lbracket)
+	return point + 1
+
 def paredit_split_sexp(view, edit):
-	pass
+	def f(region):
+		if not region.a == region.b:
+			return region
+
+		point = region.a
+
+		(lb, rb) = shared.get_expression(view, point)
+
+		if lb and rb:
+			lc = view.substr(lb)
+			rc = view.substr(rb - 1)
+			spaces_start = shared.remove_spaces(view, edit, point)
+			return insert_split_brackets(view, edit, lc, rc, spaces_start)
+
+		return point
+
+	shared.edit_selections(view, f)
 
 def paredit_join_sexp(view, edit):
 	pass
