c1c659f19489857cdc3f20d51099c745fd63e3ac
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-05-19 20:15:25 +0200

    refactored paredit_kill_abstract

--- deleting_killing.py
+++ deleting_killing.py
@@ -87,20 +87,21 @@ def paredit_backward_delete(view, edit):
 
 def paredit_kill_abstract(view, edit, expression):
 	def f(region):
-		point = region.begin()
-		if region.end() == point:
-			(lb, rb) = shared.get_expression(view, point)
-			if lb and rb:
-				if expression:
-					view.erase(edit, sublime.Region(lb + 1, rb - 1))
-					return lb + 1
-				else:
-					view.erase(edit, sublime.Region(point, rb - 1))
-					return point
+		if region.a == region.end:
+			return region
+
+		point = region.a
+
+		(lb, rb) = shared.get_expression(view, point)
+		if lb and rb:
+			if expression:
+				view.erase(edit, sublime.Region(lb + 1, rb - 1))
+				return lb + 1
 			else:
+				view.erase(edit, sublime.Region(point, rb - 1))
 				return point
 		else:
-			return region
+			return point
 
 	shared.edit_selections(view, f)
 
