4afbc4e1a2561cfc8c2e2212381eab2a0c6663ca
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-05-20 00:28:50 +0200

    stubbed paredit_join/split_sexp

--- miscellaneous.py
+++ miscellaneous.py
@@ -5,4 +5,18 @@ try:
 except:
 	import shared
 
+def paredit_split_sexp(view, edit):
+	pass
 
+def paredit_join_sexp(view, edit):
+	pass
+
+class Paredit_split_sexpCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		if shared.should_paredit(self.view):
+			paredit_split_sexp(self.view, edit)
+
+class Paredit_join_sexpCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		if shared.should_paredit(self.view):
+			paredit_join_sexp(self.view, edit)
