8a09ae3562668e32523a3ae52cb31cea51ed9d60
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-05-20 12:26:47 +0200

    paredit_forward_slurp_sexp

--- barfage_slurpage.py
+++ barfage_slurpage.py
@@ -6,7 +6,23 @@ except:
 	import shared
 
 def paredit_forward_slurp_sexp(view, edit):
-	pass
+	def f(region):
+		if not region.a == region.b:
+			return region
+		point = region.a
+
+		(l_exp, r_exp) = shared.get_expression(view, point)
+
+		if l_exp and r_exp:
+			(lnext_exp, rnext_exp) = shared.get_next_expression(view, r_exp, True)
+			if lnext_exp and rnext_exp:
+				rbracket = view.substr(r_exp - 1)
+				view.erase(edit, sublime.Region(r_exp - 1, r_exp))
+				view.insert(edit, rnext_exp - 1, rbracket)
+
+		return point
+
+	shared.edit_selections(view, f)
 
 def paredit_forward_barf_sexp(view, edit):
 	pass
