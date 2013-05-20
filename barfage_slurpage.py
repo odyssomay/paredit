e49cb61cd42cd75f6fdffd18f4a7c56b0ffeef44
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-05-20 12:28:23 +0200

    paredit_forward_barf_sexp implementation

--- barfage_slurpage.py
+++ barfage_slurpage.py
@@ -25,7 +25,23 @@ def paredit_forward_slurp_sexp(view, edit):
 	shared.edit_selections(view, f)
 
 def paredit_forward_barf_sexp(view, edit):
-	pass
+	def f(region):
+		if not region.a == region.b:
+			return region
+		point = region.a
+
+		(l_exp, r_exp) = shared.get_expression(view, point)
+
+		if l_exp and r_exp:
+			(lnext_exp, rnext_exp) = shared.get_next_expression(view, point, True)
+			if lnext_exp and rnext_exp:
+				rbracket = view.substr(r_exp - 1)
+				view.erase(edit, sublime.Region(r_exp - 1, r_exp))
+				view.insert(edit, rnext_exp, rbracket)
+
+		return point
+
+	shared.edit_selections(view, f)
 
 def paredit_backward_slurp_sexp(view, edit):
 	pass
