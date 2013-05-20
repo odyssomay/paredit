06c204171ea0997fc191d53c5d25f420d1cb9265
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-05-20 12:05:21 +0200

    paredit_join_sexp implementation

--- miscellaneous.py
+++ miscellaneous.py
@@ -13,7 +13,6 @@ def paredit_split_sexp(view, edit):
 	def f(region):
 		if not region.a == region.b:
 			return region
-
 		point = region.a
 
 		(lb, rb) = shared.get_expression(view, point)
@@ -28,8 +27,31 @@ def paredit_split_sexp(view, edit):
 
 	shared.edit_selections(view, f)
 
+def join_sexp(view, edit, prev_i, next_i, should_insert_space=True):
+	view.erase(edit, sublime.Region(prev_i, next_i + 1))
+	if should_insert_space:
+		view.insert(edit, prev_i, " ")
+	return prev_i
+
 def paredit_join_sexp(view, edit):
-	pass
+	def f(region):
+		if not region.a == region.b:
+			return region
+		point = region.a
+
+		(prev_i, prev_char) = shared.get_previous_character(view, point)
+		(next_i, next_char) = shared.get_next_character(view, point)
+
+		if (prev_char == ")" and next_char == "(" or
+		    prev_char == "]" and next_char == "[" or
+		    prev_char == "}" and next_char == "{"):
+			return join_sexp(view, edit, prev_i, next_i)
+		elif prev_char == "\"" and next_char == "\"":
+			return join_sexp(view, edit, prev_i, next_i, False)
+		else:
+			return shared.remove_spaces(view, edit, point)
+
+	shared.edit_selections(view, f)
 
 class Paredit_split_sexpCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
