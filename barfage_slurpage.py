78799c57dd58f68cd535396e782b2c903c7ae4f6
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-06-02 15:50:58 +0200

    barfage now only barfs one expression (issue #5)

--- barfage_slurpage.py
+++ barfage_slurpage.py
@@ -43,12 +43,19 @@ def paredit_barf_sexp(view, edit, direction):
 		(a_exp, b_exp) = shared.get_expression(view, point, direction)
 
 		if a_exp and b_exp:
-			(anext_exp, bnext_exp) = shared.get_next_expression(view, point, True, direction)
+			(anext_exp, bnext_exp) = reversed(shared.get_next_expression(
+				view, shared.step(b_exp, -1 if direction == "backward" else -2, direction), True, shared.opposite_direction(direction)))
 			if anext_exp and bnext_exp:
 				end_bracket = shared.get_char(view, shared.step(b_exp, -1, direction), direction)
 
 				view.erase(edit, sublime.Region(shared.step(b_exp, -1, direction), b_exp))
-				shared.insert(view, edit, bnext_exp, end_bracket, direction)
+				target_point = anext_exp
+				for (i, c) in shared.walk(view, shared.step(anext_exp, -1 if direction == "forward" else -2, direction), shared.opposite_direction(direction)):
+					if not c.isspace():
+						target_point = shared.step(i, 1, direction)
+						break
+
+				shared.insert(view, edit, target_point, end_bracket, direction)
 
 		return point
 
