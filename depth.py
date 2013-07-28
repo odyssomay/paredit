0feb52c5407a9da0cf3d3e4876fb58cbc1d9bd49
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-07-29 01:30:53 +0200

    paredit-splice-sexp-killing-backward implementation

--- depth.py
+++ depth.py
@@ -55,6 +55,23 @@ def paredit_splice_sexp(view, edit):
 
 	shared.edit_selections(view, f)
 
+def paredit_splice_sexp_killing_backward(view, edit):
+	def f(region):
+		if not region.a == region.b:
+			return region
+
+		point = region.a
+
+		(lb, rb) = shared.get_expression(view, point)
+		if shared.truthy(lb, rb):
+			view.erase(edit, sublime.Region(rb - 1, rb))
+			view.erase(edit, sublime.Region(lb, point))
+			return lb
+		else:
+			return point
+
+	shared.edit_selections(view, f)
+
 ####
 #### Commands
 class Paredit_wrap_roundCommand(sublime_plugin.TextCommand):
@@ -76,3 +93,7 @@ class Paredit_meta_doublequoteCommand(sublime_plugin.TextCommand):
 class Paredit_splice_sexpCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		paredit_splice_sexp(self.view, edit)
+
+class Paredit_splice_sexp_killing_backwardCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		paredit_splice_sexp_killing_backward(self.view, edit)
